20951de29dd0c8851680e806712076596104f65f
Jonathan <cypherirl@gmail.com>
2025-06-05 16:25:55 -0700

    Add button that allows user to specify grid size

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+// Add button to choose grid size.
+const gridSizeButton = document.createElement('button');
+gridSizeButton.textContent = 'Choose Grid Size';
+gridSizeButton.classList.add('grid-size-button');
+document.body.appendChild(gridSizeButton);
+
+let gridSize = 16;
+let totalGridItems = 256;
+
+// Prompt the user to choose a grid size.
+gridSizeButton.addEventListener('click', () => {
+  gridSize = prompt('Choose a grid size! Max size is 100x100.')
+  totalGridItems = gridSize * gridSize;
+});
+
+// Generate grid container.
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
 document.body.appendChild(gridContainer);
 
-for (let i = 0; i < 256; i++) {
+// Generate grid elements.
+for (let i = 0; i < totalGridItems; i++) {
   const gridItem = document.createElement('div');
   gridItem.classList.add('grid-item');
   gridContainer.appendChild(gridItem);
